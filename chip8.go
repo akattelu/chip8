66c54818e6b1bd82a6448df31ded9373d9384cca
Aakash Kattelu <aakash.kattelu@gmail.com>
2024-06-15 22:03:05 -0700

    Refactor field names

---
+++ chip8.go
@@ -0,0 +1,386 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+)
+
+type CHIP8 struct {
+	registers      [16]byte
+	soundTimer     byte
+	delayTimer     byte
+	indexRegister  uint16
+	programCounter uint16 // only really needs 12 bits (programs are 4096 bytes)
+	memory         [4096]byte
+	stack          [16]uint16
+	sp             byte
+
+	Graphics      [64 * 32]byte
+	KeyState      [16]bool
+	WaitingForKey *byte
+}
+
+func NewCHIP8() CHIP8 {
+	var registers [16]byte
+	return CHIP8{
+		registers:      registers,
+		programCounter: 0x200, // start of most programs
+		memory:         NewMemory(),
+	}
+}
+
+
+func (c *CHIP8) FinishWaitingForKey(sdlKey int) {
+	if c.WaitingForKey != nil {
+		loc, ok := KEY_STATE_MAP[sdlKey]
+		if ok {
+			c.registers[*c.WaitingForKey] = loc
+			c.WaitingForKey = nil
+		}
+	}
+}
+
+func (c *CHIP8) PressKey(sdlKey int) {
+	loc, ok := KEY_STATE_MAP[sdlKey]
+	if ok {
+		c.KeyState[loc] = true
+	}
+}
+
+func (c *CHIP8) ReleaseKey(sdlKey int) {
+	loc, ok := KEY_STATE_MAP[sdlKey]
+	if ok {
+		c.KeyState[loc] = false
+	}
+}
+
+
+
+func (c *CHIP8) LoadROM(romBytes []byte) {
+	for i, b := range romBytes {
+		c.memory[0x200+i] = b
+	}
+}
+
+func (c *CHIP8) ReadInstruction() uint16 {
+	first := c.memory[c.programCounter]
+	second := c.memory[c.programCounter+1]
+	return binary.BigEndian.Uint16([]byte{first, second})
+}
+
+
+
+
+
+
+func (c *CHIP8) Run(instr uint16) {
+	// just exact codes
+	switch instr {
+	case 0x00E0:
+		c.clearDisplay()
+	case 0x00EE:
+		c.programCounter = c.stack[c.sp]
+		c.sp -= 1
+	}
+	opcode := instr & 0xF000
+	switch opcode {
+	case 0x1000: // jump
+		c.programCounter = instr & 0x0FFF
+		return
+	case 0x2000: // call
+		c.sp += 1
+		c.stack[c.sp] = c.programCounter
+		c.programCounter = instr & 0x0FFF
+		return
+	case 0x3000: // skip if equal
+		reg := (instr & 0x0F00) >> 8
+		left := c.registers[reg]
+		right := byte(instr & 0x00FF)
+		if left == right {
+			c.advance()
+		}
+	case 0x4000: // skip if not equal
+		reg := (instr & 0x0F00) >> 8
+		left := c.registers[reg]
+		right := byte(instr & 0x00FF)
+		if left != right {
+			c.advance()
+		}
+	case 0x5000: // skip if registers equal
+		left := c.registers[(instr&0x0F00)>>8]
+		right := c.registers[(instr&0x00F0)>>4]
+		if left == right {
+			c.advance()
+		}
+	case 0x6000: // load into register
+		reg := (instr & 0x0F00) >> 8
+		c.registers[reg] = byte(instr & 0x00FF)
+	case 0x7000: // add
+		reg := (instr & 0x0F00) >> 8
+		c.registers[reg] = c.registers[reg] + (byte(instr & 0x00FF))
+	case 0x8000:
+		switch instr & 0x000F {
+		case 0x0000: // set left to right
+			left := (instr & 0x0F00) >> 8
+			right := (instr & 0x00F0) >> 4
+			c.registers[left] = c.registers[right]
+		case 0x0001: // bitwise or
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			c.registers[leftReg] = c.registers[leftReg] | c.registers[rightReg]
+			c.registers[0xF] = 0
+		case 0x0002: // bitwise and
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			c.registers[leftReg] = c.registers[leftReg] & c.registers[rightReg]
+			c.registers[0xF] = 0
+		case 0x0003: // bitwise xor
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			c.registers[leftReg] = c.registers[leftReg] ^ c.registers[rightReg]
+			c.registers[0xF] = 0
+		case 0x0004: // add with carry
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			result16 := uint16(c.registers[leftReg]) + uint16(c.registers[rightReg])
+			c.registers[leftReg] = byte(result16 & 0x00FF)
+			if result16&0xFF00 > 0 {
+				c.registers[0xF] = 1
+			} else {
+				c.registers[0xF] = 0
+			}
+		case 0x0005: // sub with borrow
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			left := c.registers[leftReg]
+			right := c.registers[rightReg]
+			var overflow byte
+			if left >= right {
+				overflow = 1
+			} else {
+				overflow = 0
+			}
+			c.registers[leftReg] = (left - right)
+			c.registers[0xF] = overflow
+		case 0x0006: // shift right
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			c.registers[leftReg] = c.registers[rightReg]
+			var overflow byte
+			if c.registers[leftReg]&0x01 == 0x01 {
+				overflow = 1
+			} else {
+				overflow = 0
+			}
+			c.registers[leftReg] >>= 1
+			c.registers[0xF] = overflow
+		case 0x0007: // sub with borrow other order
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			left := uint16(c.registers[leftReg])
+			right := uint16(c.registers[rightReg])
+			var overflow byte
+			if right >= left {
+				overflow = 1
+			} else {
+				overflow = 0
+			}
+			c.registers[leftReg] = byte((right - left) & 0x00FF)
+			c.registers[0xF] = overflow
+		case 0x000E:
+			leftReg := instr & 0x0F00 >> 8
+			rightReg := instr & 0x00F0 >> 4
+			c.registers[leftReg] = c.registers[rightReg]
+			var overflow byte
+			if c.registers[leftReg]&0x80 == 0x80 {
+				overflow = 1
+			} else {
+				overflow = 0
+			}
+			c.registers[leftReg] *= 2
+			c.registers[0xF] = overflow
+
+		}
+	case 0x9000: // skip if registers not equal
+		left := c.registers[(instr&0x0F00)>>8]
+		right := c.registers[(instr&0x00F0)>>4]
+		if left != right {
+			c.advance()
+		}
+	case 0xA000: // set index
+		c.indexRegister = instr & 0x0FFF
+	case 0xB000: // increment pc by v0
+		c.programCounter = uint16(c.registers[0]) + instr&0x0FFF
+		return
+	case 0xC000: // random mask
+		randBytes := [2]byte{}
+		_, err := rand.Read(randBytes[:])
+		if err != nil {
+			panic(err)
+		}
+		rand16 := binary.BigEndian.Uint16(randBytes[:]) & 0x00FF
+		c.registers[(instr&0x0F00)>>8] = byte(rand16 & (instr & 0x00FF))
+	case 0xD000: // draw
+		left := c.registers[(instr&0x0F00)>>8]
+		right := c.registers[(instr&0x00F0)>>4]
+		numBytes := instr & 0x000F
+		memStart := c.indexRegister
+		sprite := c.memory[memStart : memStart+numBytes]
+		if overflow := c.writeGraphics(left, right, sprite); overflow {
+			c.registers[0xF] = 1
+		} else {
+			c.registers[0xF] = 0
+		}
+	case 0xE000:
+		switch byte(instr & 0x00FF) {
+		case 0x009E: // Skip if Vx value key press
+			reg := (instr & 0x0F00) >> 8
+			if c.KeyState[c.registers[reg]] {
+				c.advance()
+			}
+		case 0x00A1: // Skip if Vx value key not pressed
+			reg := (instr & 0x0F00) >> 8
+			if !c.KeyState[c.registers[reg]] {
+				c.advance()
+			}
+		}
+	case 0xF000:
+		switch byte(instr & 0x00FF) {
+		case 0x0007: // read delay timer
+			reg := byte((instr & 0x0F00) >> 8)
+			c.registers[reg] = c.delayTimer
+		case 0x000A: // wait for key press
+			reg := byte((instr & 0x0F00) >> 8)
+			c.WaitingForKey = &reg
+		case 0x0015: // load delay timer
+			reg := byte((instr & 0x0F00) >> 8)
+			c.delayTimer = c.registers[reg]
+		case 0x0018: // set sound timer
+			reg := byte((instr & 0x0F00) >> 8)
+			c.soundTimer = c.registers[reg]
+		case 0x001E: // I += Vx
+			reg := (instr & 0x0F00) >> 8
+			c.indexRegister += uint16(c.registers[reg]) & 0xFFF
+		case 0x0029: // load sprite
+			font := instr & 0x0F00
+			c.indexRegister = c.fontLocation(font)
+		case 0x0033:
+			reg := (instr & 0x0F00) >> 8
+			val := c.registers[reg]
+			c.memory[c.indexRegister] = val / 100
+			c.memory[c.indexRegister+1] = (val % 100) / 10
+			c.memory[c.indexRegister+2] = (val % 100) % 10
+		case 0x0055: // dump registers
+			untilRegister := (instr & 0x0F00) >> 8
+			for i, val := range c.registers[0 : untilRegister+1] {
+				c.memory[i+int(c.indexRegister)] = val
+			}
+			c.indexRegister = c.indexRegister + untilRegister + 1
+		case 0x0065: // read registers
+			untilRegister := (instr & 0x0F00) >> 8
+			for i, memVal := range c.memory[int(c.indexRegister) : int(c.indexRegister)+int(untilRegister)+1] {
+				c.registers[i] = memVal
+			}
+			c.indexRegister = c.indexRegister + untilRegister + 1
+		}
+
+	}
+	c.advance()
+}
+
+func (c *CHIP8) advance() {
+	c.programCounter += 2
+}
+
+
+
+func (c *CHIP8) fontLocation(font uint16) uint16 {
+	return 5 * font
+}
+
+func (c *CHIP8) clearDisplay() {
+	c.Graphics = [64 * 32]byte{}
+}
+
+func (c *CHIP8) writeGraphics(x, y byte, sprite []byte) bool {
+	startX := int(x) % 64
+	startY := int(y) % 32
+
+	for _, spriteByte := range sprite {
+		pos := toGraphicsPos(startX, startY)
+		c.Graphics[getWrappedPos(pos, 0)] ^= (spriteByte & 0x80) >> 7
+		c.Graphics[getWrappedPos(pos, 1)] ^= (spriteByte & 0x40) >> 6
+		c.Graphics[getWrappedPos(pos, 2)] ^= (spriteByte & 0x20) >> 5
+		c.Graphics[getWrappedPos(pos, 3)] ^= (spriteByte & 0x10) >> 4
+		c.Graphics[getWrappedPos(pos, 4)] ^= (spriteByte & 0x08) >> 3
+		c.Graphics[getWrappedPos(pos, 5)] ^= (spriteByte & 0x04) >> 2
+		c.Graphics[getWrappedPos(pos, 6)] ^= (spriteByte & 0x02) >> 1
+		c.Graphics[getWrappedPos(pos, 7)] ^= (spriteByte & 0x01) >> 0
+		startY = (startY + 1) % 32
+	}
+	return false
+}
+
+func toGraphicsPos(x, y int) int {
+	return (int(y) * 64) + int(x)
+}
+
+func fromGraphicsPos(pos int) (int, int) {
+	x_pos := pos % 64
+	y_pos := pos / 64
+	return x_pos, y_pos
+}
+
+func getWrappedPos(initial, offset int) int {
+	x, y := fromGraphicsPos(initial)
+	if x+offset > 63 {
+		return toGraphicsPos(x+offset%64, y)
+	}
+	return toGraphicsPos(x+offset, y)
+}
+
+func NewMemory() [4096]byte {
+	var arr [4096]byte
+	var sprites [][]byte = [][]byte{
+		// 0
+		{0xF0, 0x90, 0x90, 0x90, 0xF0},
+		// 1
+		{0x20, 0x60, 0x20, 0x20, 0x70},
+		// 2
+		{0xF0, 0x10, 0xF0, 0x80, 0xF0},
+		// 3
+		{0xF0, 0x10, 0xF0, 0x10, 0xF0},
+		// 4
+		{0x90, 0x90, 0xF0, 0x10, 0x10},
+		// 5
+		{0xF0, 0x80, 0xF0, 0x10, 0xF0},
+		// 6
+		{0xF0, 0x80, 0xF0, 0x90, 0xF0},
+		// 7
+		{0xF0, 0x10, 0x20, 0x40, 0x40},
+		// 8
+		{0xF0, 0x90, 0xF0, 0x90, 0xF0},
+		// 9
+		{0xF0, 0x90, 0xF0, 0x10, 0xF0},
+		// A
+		{0xF0, 0x90, 0xF0, 0x90, 0x90},
+		// B
+		{0xE0, 0x90, 0xE0, 0x90, 0xE0},
+		// C
+		{0xF0, 0x80, 0x80, 0x80, 0xF0},
+		// D
+		{0xE0, 0x90, 0x90, 0x90, 0xE0},
+		// E
+		{0xF0, 0x80, 0xF0, 0x80, 0xF0},
+		// F
+		{0xF0, 0x80, 0xF0, 0x80, 0x80},
+	}
+	addr := 0
+	for _, sprite := range sprites {
+		for _, sprite_byte := range sprite {
+			arr[addr] = sprite_byte
+			addr++
+		}
+	}
+	return arr
+}
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/binary"
-	"fmt"
-	"math"
+	"github.com/veandco/go-sdl2/sdl"
 	"os"
 	"time"
-
-	"github.com/veandco/go-sdl2/sdl"
+	"math"
 )
 
 const (
@@ -48,8 +44,8 @@ func main() {
 		panic(err)
 	}
 
-	cpu := NewCPU()
-	cpu.LoadROM(romBytes)
+	chip8 := NewCHIP8()
+	chip8.LoadROM(romBytes)
 
 	window, err := sdl.CreateWindow("chip8-go", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		64*PIXEL_SIZE, 32*PIXEL_SIZE, sdl.WINDOW_SHOWN)
@@ -80,7 +76,7 @@ func main() {
 	bgColor := sdl.Color{R: 0x4c, G: 0x56, B: 0x6a, A: 255}
 	running := true
 	for running {
-		for i, pixel := range cpu.Graphics {
+		for i, pixel := range chip8.Graphics {
 			x, y := fromGraphicsPos(i)
 
 			var colorVal uint32
@@ -94,18 +90,18 @@ func main() {
 		}
 		window.UpdateSurface()
 
-		instruction := cpu.ReadInstruction()
-		if cpu.waitingForKey == nil {
-			cpu.Run(instruction)
+		instruction := chip8.ReadInstruction()
+		if chip8.WaitingForKey == nil {
+			chip8.Run(instruction)
 		}
-		if cpu.DelayTimer > 0 || cpu.SoundTimer > 0 {
-			if cpu.DelayTimer > 0 {
-				cpu.DelayTimer -= 1
+		if chip8.delayTimer > 0 || chip8.soundTimer > 0 {
+			if chip8.delayTimer > 0 {
+				chip8.delayTimer -= 1
 
 			}
-			if cpu.SoundTimer > 0 {
-				cpu.PlaySound(deviceID)
-				cpu.SoundTimer -= 1
+			if chip8.soundTimer > 0 {
+				PlaySound(deviceID)
+				chip8.soundTimer -= 1
 			}
 			time.Sleep(time.Second / 60)
 		}
@@ -113,17 +109,16 @@ func main() {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
 			case *sdl.QuitEvent:
-				println("Quit")
 				running = false
 				break
 			case *sdl.KeyboardEvent:
 				if t.Type == sdl.KEYDOWN {
-					cpu.PressKey(int(t.Keysym.Sym))
+					chip8.PressKey(int(t.Keysym.Sym))
 				}
 				if t.Type == sdl.KEYUP {
-					cpu.ReleaseKey(int(t.Keysym.Sym))
-					if cpu.waitingForKey != nil {
-						cpu.FinishWaitingForKey(int(t.Keysym.Sym))
+					chip8.ReleaseKey(int(t.Keysym.Sym))
+					if chip8.WaitingForKey != nil {
+						chip8.FinishWaitingForKey(int(t.Keysym.Sym))
 					}
 				}
 			}
@@ -131,34 +126,7 @@ func main() {
 	}
 }
 
-type CPU struct {
-	Reg            [16]byte
-	SoundTimer     byte
-	DelayTimer     byte
-	IndexRegister  uint16
-	ProgramCounter uint16 // only really needs 12 bits (programs are 4096 bytes)
-	Memory         Memory
-	Graphics       [64 * 32]byte
-	Stack          [16]uint16
-	sp             byte
-	KeyState       [16]bool
-	waitingForKey  *byte
-}
-
-type Memory [4096]byte
-
-func NewCPU() CPU {
-	var registers [16]byte
-	return CPU{
-		Reg:            registers,
-		SoundTimer:     0,
-		DelayTimer:     0,
-		ProgramCounter: 0x200, // start of most programs
-		Memory:         NewMemory(),
-	}
-}
-
-func (c *CPU) PlaySound(deviceID sdl.AudioDeviceID) {
+func PlaySound(deviceID sdl.AudioDeviceID) {
 	durationSeconds := 1.0 / 60.0
 	samples := int(float64(AUDIO_SAMPLE_RATE) * durationSeconds)
 	stream := make([]float32, samples)
@@ -176,350 +144,4 @@ func (c *CPU) PlaySound(deviceID sdl.AudioDeviceID) {
 	}
 
 	sdl.QueueAudio(deviceID, bytes)
-}
-
-func (c *CPU) FinishWaitingForKey(sdlKey int) {
-	if c.waitingForKey != nil {
-		loc, ok := KEY_STATE_MAP[sdlKey]
-		if ok {
-			c.Reg[*c.waitingForKey] = loc
-			c.waitingForKey = nil
-		}
-	}
-}
-
-func (c *CPU) PressKey(sdlKey int) {
-	loc, ok := KEY_STATE_MAP[sdlKey]
-	if ok {
-		c.KeyState[loc] = true
-	}
-}
-
-func (c *CPU) ReleaseKey(sdlKey int) {
-	loc, ok := KEY_STATE_MAP[sdlKey]
-	if ok {
-		c.KeyState[loc] = false
-	}
-}
-
-func (c *CPU) FontLocation(font uint16) uint16 {
-	return 5 * font
-}
-
-func (c *CPU) LoadROM(romBytes []byte) {
-	for i, b := range romBytes {
-		c.Memory[0x200+i] = b
-	}
-}
-
-func (c *CPU) ReadInstruction() uint16 {
-	first := c.Memory[c.ProgramCounter]
-	second := c.Memory[c.ProgramCounter+1]
-	return binary.BigEndian.Uint16([]byte{first, second})
-}
-
-func (c *CPU) ClearDisplay() {
-	c.Graphics = [64 * 32]byte{}
-}
-
-func ToGraphicsPos(x, y int) int {
-	return (int(y) * 64) + int(x)
-}
-
-func fromGraphicsPos(pos int) (int, int) {
-	x_pos := pos % 64
-	y_pos := pos / 64
-	return x_pos, y_pos
-}
-
-func GetWrappedPos(initial, offset int) int {
-	x, y := fromGraphicsPos(initial)
-	if x+offset > 63 {
-		return ToGraphicsPos(x+offset%64, y)
-	}
-	return ToGraphicsPos(x + offset, y)
-}
-
-func (c *CPU) WriteGraphics(x, y byte, sprite []byte) bool {
-	startX := int(x) % 64
-	startY := int(y) % 32
-
-	for _, spriteByte := range sprite {
-		pos := ToGraphicsPos(startX, startY)
-		c.Graphics[GetWrappedPos(pos, 0)] ^= (spriteByte & 0x80) >> 7
-		c.Graphics[GetWrappedPos(pos, 1)] ^= (spriteByte & 0x40) >> 6
-		c.Graphics[GetWrappedPos(pos, 2)] ^= (spriteByte & 0x20) >> 5
-		c.Graphics[GetWrappedPos(pos, 3)] ^= (spriteByte & 0x10) >> 4
-		c.Graphics[GetWrappedPos(pos, 4)] ^= (spriteByte & 0x08) >> 3
-		c.Graphics[GetWrappedPos(pos, 5)] ^= (spriteByte & 0x04) >> 2
-		c.Graphics[GetWrappedPos(pos, 6)] ^= (spriteByte & 0x02) >> 1
-		c.Graphics[GetWrappedPos(pos, 7)] ^= (spriteByte & 0x01) >> 0
-		startY = (startY + 1) % 32
-	}
-	return false
-}
-
-func (c *CPU) Run(instr uint16) {
-	// just exact codes
-	switch instr {
-	case 0x00E0:
-		c.ClearDisplay()
-	case 0x00EE:
-		c.ProgramCounter = c.Stack[c.sp]
-		c.sp -= 1
-	}
-	opcode := instr & 0xF000
-	switch opcode {
-	case 0x1000: // jump
-		c.ProgramCounter = instr & 0x0FFF
-		return
-	case 0x2000: // call
-		c.sp += 1
-		c.Stack[c.sp] = c.ProgramCounter
-		c.ProgramCounter = instr & 0x0FFF
-		return
-	case 0x3000: // skip if equal
-		reg := (instr & 0x0F00) >> 8
-		left := c.Reg[reg]
-		right := byte(instr & 0x00FF)
-		if left == right {
-			c.Advance()
-		}
-	case 0x4000: // skip if not equal
-		reg := (instr & 0x0F00) >> 8
-		left := c.Reg[reg]
-		right := byte(instr & 0x00FF)
-		if left != right {
-			c.Advance()
-		}
-	case 0x5000: // skip if registers equal
-		left := c.Reg[(instr&0x0F00)>>8]
-		right := c.Reg[(instr&0x00F0)>>4]
-		if left == right {
-			c.Advance()
-		}
-	case 0x6000: // load into register
-		reg := (instr & 0x0F00) >> 8
-		c.Reg[reg] = byte(instr & 0x00FF)
-	case 0x7000: // add
-		reg := (instr & 0x0F00) >> 8
-		c.Reg[reg] = c.Reg[reg] + (byte(instr & 0x00FF))
-	case 0x8000:
-		switch instr & 0x000F {
-		case 0x0000: // set left to right
-			left := (instr & 0x0F00) >> 8
-			right := (instr & 0x00F0) >> 4
-			c.Reg[left] = c.Reg[right]
-		case 0x0001: // bitwise or
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			c.Reg[leftReg] = c.Reg[leftReg] | c.Reg[rightReg]
-			c.Reg[0xF] = 0
-		case 0x0002: // bitwise and
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			c.Reg[leftReg] = c.Reg[leftReg] & c.Reg[rightReg]
-			c.Reg[0xF] = 0
-		case 0x0003: // bitwise xor
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			c.Reg[leftReg] = c.Reg[leftReg] ^ c.Reg[rightReg]
-			c.Reg[0xF] = 0
-		case 0x0004: // add with carry
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			result16 := uint16(c.Reg[leftReg]) + uint16(c.Reg[rightReg])
-			c.Reg[leftReg] = byte(result16 & 0x00FF)
-			if result16&0xFF00 > 0 {
-				c.Reg[0xF] = 1
-			} else {
-				c.Reg[0xF] = 0
-			}
-		case 0x0005: // sub with borrow
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			left := c.Reg[leftReg]
-			right := c.Reg[rightReg]
-			var overflow byte
-			if left >= right {
-				overflow = 1
-			} else {
-				overflow = 0
-			}
-			c.Reg[leftReg] = (left - right)
-			c.Reg[0xF] = overflow
-		case 0x0006: // shift right
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			c.Reg[leftReg] = c.Reg[rightReg]
-			var overflow byte
-			if c.Reg[leftReg]&0x01 == 0x01 {
-				overflow = 1
-			} else {
-				overflow = 0
-			}
-			c.Reg[leftReg] >>= 1
-			c.Reg[0xF] = overflow
-		case 0x0007: // sub with borrow other order
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			left := uint16(c.Reg[leftReg])
-			right := uint16(c.Reg[rightReg])
-			var overflow byte
-			if right >= left {
-				overflow = 1
-			} else {
-				overflow = 0
-			}
-			c.Reg[leftReg] = byte((right - left) & 0x00FF)
-			c.Reg[0xF] = overflow
-		case 0x000E:
-			leftReg := instr & 0x0F00 >> 8
-			rightReg := instr & 0x00F0 >> 4
-			c.Reg[leftReg] = c.Reg[rightReg]
-			var overflow byte
-			if c.Reg[leftReg]&0x80 == 0x80 {
-				overflow = 1
-			} else {
-				overflow = 0
-			}
-			c.Reg[leftReg] *= 2
-			c.Reg[0xF] = overflow
-
-		}
-	case 0x9000: // skip if registers not equal
-		left := c.Reg[(instr&0x0F00)>>8]
-		right := c.Reg[(instr&0x00F0)>>4]
-		if left != right {
-			c.Advance()
-		}
-	case 0xA000: // set index
-		c.IndexRegister = instr & 0x0FFF
-	case 0xB000: // increment pc by v0
-		c.ProgramCounter = uint16(c.Reg[0]) + instr&0x0FFF
-		return
-	case 0xC000: // random mask
-		randBytes := [2]byte{}
-		_, err := rand.Read(randBytes[:])
-		if err != nil {
-			panic(err)
-		}
-		rand16 := binary.BigEndian.Uint16(randBytes[:]) & 0x00FF
-		c.Reg[(instr&0x0F00)>>8] = byte(rand16 & (instr & 0x00FF))
-	case 0xD000: // draw
-		left := c.Reg[(instr&0x0F00)>>8]
-		right := c.Reg[(instr&0x00F0)>>4]
-		numBytes := instr & 0x000F
-		memStart := c.IndexRegister
-		sprite := c.Memory[memStart : memStart+numBytes]
-		if overflow := c.WriteGraphics(left, right, sprite); overflow {
-			c.Reg[0xF] = 1
-		} else {
-			c.Reg[0xF] = 0
-		}
-	case 0xE000:
-		switch byte(instr & 0x00FF) {
-		case 0x009E: // Skip if Vx value key press
-			reg := (instr & 0x0F00) >> 8
-			if c.KeyState[c.Reg[reg]] {
-				c.Advance()
-			}
-		case 0x00A1: // Skip if Vx value key not pressed
-			reg := (instr & 0x0F00) >> 8
-			if !c.KeyState[c.Reg[reg]] {
-				c.Advance()
-			}
-		}
-	case 0xF000:
-		switch byte(instr & 0x00FF) {
-		case 0x0007: // read delay timer
-			reg := byte((instr & 0x0F00) >> 8)
-			c.Reg[reg] = c.DelayTimer
-		case 0x000A: // wait for key press
-			reg := byte((instr & 0x0F00) >> 8)
-			c.waitingForKey = &reg
-		case 0x0015: // load delay timer
-			reg := byte((instr & 0x0F00) >> 8)
-			c.DelayTimer = c.Reg[reg]
-		case 0x0018: // set sound timer
-			reg := byte((instr & 0x0F00) >> 8)
-			c.SoundTimer = c.Reg[reg]
-		case 0x001E: // I += Vx
-			reg := (instr & 0x0F00) >> 8
-			c.IndexRegister += uint16(c.Reg[reg]) & 0xFFF
-		case 0x0029: // load sprite
-			font := instr & 0x0F00
-			c.IndexRegister = c.FontLocation(font)
-		case 0x0033:
-			reg := (instr & 0x0F00) >> 8
-			val := c.Reg[reg]
-			c.Memory[c.IndexRegister] = val / 100
-			c.Memory[c.IndexRegister+1] = (val % 100) / 10
-			c.Memory[c.IndexRegister+2] = (val % 100) % 10
-		case 0x0055: // dump registers
-			untilRegister := (instr & 0x0F00) >> 8
-			for i, val := range c.Reg[0 : untilRegister+1] {
-				c.Memory[i+int(c.IndexRegister)] = val
-			}
-			c.IndexRegister = c.IndexRegister + untilRegister + 1
-		case 0x0065: // read registers
-			untilRegister := (instr & 0x0F00) >> 8
-			for i, memVal := range c.Memory[int(c.IndexRegister) : int(c.IndexRegister)+int(untilRegister)+1] {
-				c.Reg[i] = memVal
-			}
-			c.IndexRegister = c.IndexRegister + untilRegister + 1
-		}
-
-	}
-	c.Advance()
-}
-
-func (c *CPU) Advance() {
-	c.ProgramCounter += 2
-}
-
-func NewMemory() Memory {
-	var arr [4096]byte
-	var sprites [][]byte = [][]byte{
-		// 0
-		{0xF0, 0x90, 0x90, 0x90, 0xF0},
-		// 1
-		{0x20, 0x60, 0x20, 0x20, 0x70},
-		// 2
-		{0xF0, 0x10, 0xF0, 0x80, 0xF0},
-		// 3
-		{0xF0, 0x10, 0xF0, 0x10, 0xF0},
-		// 4
-		{0x90, 0x90, 0xF0, 0x10, 0x10},
-		// 5
-		{0xF0, 0x80, 0xF0, 0x10, 0xF0},
-		// 6
-		{0xF0, 0x80, 0xF0, 0x90, 0xF0},
-		// 7
-		{0xF0, 0x10, 0x20, 0x40, 0x40},
-		// 8
-		{0xF0, 0x90, 0xF0, 0x90, 0xF0},
-		// 9
-		{0xF0, 0x90, 0xF0, 0x10, 0xF0},
-		// A
-		{0xF0, 0x90, 0xF0, 0x90, 0x90},
-		// B
-		{0xE0, 0x90, 0xE0, 0x90, 0xE0},
-		// C
-		{0xF0, 0x80, 0x80, 0x80, 0xF0},
-		// D
-		{0xE0, 0x90, 0x90, 0x90, 0xE0},
-		// E
-		{0xF0, 0x80, 0xF0, 0x80, 0xF0},
-		// F
-		{0xF0, 0x80, 0xF0, 0x80, 0x80},
-	}
-	addr := 0
-	for _, sprite := range sprites {
-		for _, sprite_byte := range sprite {
-			arr[addr] = sprite_byte
-			addr++
-		}
-	}
-	return arr
 }
